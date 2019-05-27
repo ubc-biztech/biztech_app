d5e325e1e85e959f69169085f283fd825728904b
Cris <cris12we4@gmail.com>
2019-05-26 21:16:29 -0700

    Initial Commit

---
+++ BizTechApp/App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.instructions}>BizTech App 2019 Summer</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	}
+});
