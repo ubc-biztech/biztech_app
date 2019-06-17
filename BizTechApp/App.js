67a7262392cd06471a7e09a725b0f687fe72a2e9
jacqueschen1 <jacqueschen1@gmail.com>
2019-06-16 22:42:45 -0400

    Updated gitignore

--- BizTechApp/App.js
+++ BizTechApp/App.js
@@ -6,39 +6,39 @@
  * @flow
  */
 
-// import React, {Component} from 'react';
-// import {Platform, StyleSheet, Text, View} from 'react-native';
-
-// export default class App extends Component {
-// 	render() {
-// 		return (
-// 			<View style={styles.container}>
-// 				<Text style={styles.instructions}>BizTech App 2019 Summer</Text>
-// 			</View>
-// 		);
-// 	}
-// }
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		justifyContent: 'center',
-// 		alignItems: 'center',
-// 		backgroundColor: '#F5FCFF'
-// 	},
-// 	instructions: {
-// 		textAlign: 'center',
-// 		color: '#333333',
-// 		marginBottom: 5
-// 	}
-// });
-
-import React, { Component } from 'react';
-
-import EmojiDict from './src/components/EmojiDict';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
 export default class App extends Component {
 	render() {
-		return <EmojiDict />;
+		return (
+			<View style={styles.container}>
+				<Text style={styles.instructions}>BizTech App 209 Summer</Text>
+			</View>
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	}
+});
+
+// import React, { Component } from 'react';
+
+// import EmojiDict from './src/components/Emoji';
+
+// export default class App extends Component {
+	// render() {
+		// return <EmojiDict />;
+	// }
+// }
