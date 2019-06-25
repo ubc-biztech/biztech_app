b3a4b44d7b803d8e812721efc099aa4321c8b6ab
Ian <ianmmah@gmail.com>
2019-06-25 01:32:42 -0700

    convert app.js to main.js

--- BizTechApp/App.js
+++ BizTechApp/App.js
@@ -1,50 +1,14 @@
 import React, { Component } from "react";
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Button,
- 				 Header,
-				 ThemeProvider } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Profile from './App/components/Profile';
-import CreateEvent from './App/components/CreateEvent';
 import Navigator from './App/features/Navigator';
 
-const theme = {
-  colors: {
-    primary: '#7ad040',
-  }
-}
 
 class App extends Component {
 	render() {
     // const {navigate} = this.props.navigation;
 		return (
-			// <ThemeProvider theme={theme}>
-			// 	<Header
-			// 		statusBarProps={{ barStyle: 'light-content', translucent: true, backgroundColor: 'transparent' }}
-			// 		leftComponent={{ icon: 'menu', color: '#fff' }}
-			// 		centerComponent={{ text: 'BizTech App', style: { color: '#fff' } }}
-			// 		rightComponent={{ icon: 'home', color: '#fff' }}
-			// 		/>
-			// 	<View>
-			// 		<View style={styles.widgetContainer}><Profile/></View>
-			// 		<View style={styles.widgetContainer}><CreateEvent theme={theme}/></View>
-			// 	</View>
-			// </ThemeProvider>
 				<Navigator/>
 		);
 	}
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF'
-	},
-	widgetContainer: {
-		margin: 20,
-	}
-});
--- BizTechApp/App/screens/Main.js
+++ BizTechApp/App/screens/Main.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Button,
+ 				 Header,
+				 ThemeProvider } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Profile from '../components/Profile';
+import CreateEvent from '../components/CreateEvent';
+
+const theme = {
+  colors: {
+    primary: '#7ad040',
+  }
+}
 
 export default class Main extends Component {
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.instructions}>Main Page</Text>
-			</View>
+			<ThemeProvider theme={theme}>
+				<Header
+					statusBarProps={{ barStyle: 'light-content', translucent: true, backgroundColor: 'transparent' }}
+					leftComponent={{ icon: 'menu', color: '#fff' }}
+					centerComponent={{ text: 'BizTech App', style: { color: '#fff' } }}
+					rightComponent={{ icon: 'home', color: '#fff' }}
+					/>
+				<View>
+					<View style={styles.widgetContainer}><Profile/></View>
+					<View style={styles.widgetContainer}><CreateEvent theme={theme}/></View>
+				</View>
+			</ThemeProvider>
 		);
 	}
 }
@@ -22,5 +43,8 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		color: '#333333',
 		marginBottom: 5
+	},
+	widgetContainer: {
+		margin: 20,
 	}
 });
