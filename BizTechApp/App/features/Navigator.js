de351bf13686949b3aa4b2d5052cb1a5aa0fbb30
Ian <ianmmah@gmail.com>
2019-06-25 01:54:28 -0700

    basic bottom tab navigator

--- BizTechApp/App/features/Navigator.js
+++ BizTechApp/App/features/Navigator.js
@@ -1,6 +1,8 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation'
 import Main from '../screens/Main'
 import Login from '../screens/Login'
+import Icon from 'react-native-vector-icons/Feather';
 
 const routeConfig = {
   Main: {
@@ -19,6 +21,31 @@ const navigatorConfig = {
   },
 }
 
-const AppNavigator = createStackNavigator(routeConfig, navigatorConfig)
+const TabNavigator = createBottomTabNavigator({
+  Main: Main,
+  Login: Login
+},
+{
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, horizontal, tintColor }) => {
+      const { routeName } = navigation.state;
+      let IconComponent = Icon;
+      let iconName;
+      if (routeName === 'Main') {
+        iconName = 'home';
+      } else if (routeName === 'Login') {
+        iconName = 'user';
+      }
+      // You can return any component that you like here!
+      return <IconComponent name={iconName} size={25} color={tintColor} />;
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: '#7ad040',
+    inactiveTintColor: 'gray',
+  },
+})
 
-export default createAppContainer(AppNavigator);
+// const AppNavigator = createStackNavigator(routeConfig, navigatorConfig)
+
+export default createAppContainer(TabNavigator);
