0c336950b128efb20466954e7d4d945a42fa6093
superzzp <zhangzhipian1@gmail.com>
2020-03-29 23:55:33 -0400

    add Carousel component and additional stylings to the homescreen

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
-import { ScrollView, RefreshControl, ActivityIndicator, View } from 'react-native';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import {
+  ScrollView,
+  RefreshControl,
+  ActivityIndicator,
+  View
+} from "react-native";
+import { connect } from "react-redux";
 //styling
-import styles from '../styles/Styles';
-import Text from '../components/Text';
-import Button from '../components/Button';
+import styles, { colors } from "../styles/Styles";
+import Text from "../components/Text";
+import Button from "../components/Button";
 
-import Notification from '../components/Notification';
-import EventCard from '../components/EventCard';
+import Notification from "../components/Notification";
+import EventCard from "../components/EventCard";
 
-import { populateEvents } from '../actions/Login';
-<<<<<<< HEAD
-import Carousel from 'react-native-snap-carousel';
-=======
-import { fetchBackend } from '../utils';
->>>>>>> 92a3f843700354fa1bf4c372bf819898e75ccec1
+import { populateEvents } from "../actions/Login";
+import Carousel, { Pagination } from "react-native-snap-carousel";
+import { sliderWidth, itemWidth } from '../styles/SliderEntry.style';
+import { ENTRIES1, ENTRIES2 } from '../static/entries';
+import SliderEntry from '../components/SliderEntry';
+import { fetchBackend } from "../utils";
+const SLIDER_1_FIRST_ITEM = 1;
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       refreshing: false,
+      slider1ActiveSlide: SLIDER_1_FIRST_ITEM
     };
+  }
+  _renderItem({ item, index }) {
+    return <SliderEntry data={item} even={(index + 1) % 2 === 0} />;
+  }
+
+  _renderItemWithParallax({ item, index }, parallaxProps) {
+    return (
+      <SliderEntry
+        data={item}
+        even={(index + 1) % 2 === 0}
+        parallax={true}
+        parallaxProps={parallaxProps}
+      />
+    );
+  }
+
+  _renderLightItem({ item, index }) {
+    return <SliderEntry data={item} even={false} />;
+  }
+
+  _renderDarkItem({ item, index }) {
+    return <SliderEntry data={item} even={true} />;
   }
 
   _onRefresh = () => {
@@ -29,44 +58,84 @@ class Home extends Component {
     this.fetchEvents().then(() => {
       this.setState({ refreshing: false });
     });
-  }
+  };
 
   async fetchEvents() {
-    console.log('home screen fetching events')
-    fetchBackend('/events/get', 'GET')
-      .then((response) => response.json())
-      .then((response) => {
-        console.log('response AAA========>')
-        console.log(JSON.stringify(response.json));
-        this.props.populateEvents(response)
-      })
-      
+    console.log("home screen fetching events");
+    fetchBackend("/events/get", "GET")
+      .then(response => response.json())
+      .then(response => {
+        console.log("response AAB========>");
+        console.log(JSON.stringify(response));
+        this.props.populateEvents(response);
+      });
   }
 
   componentDidMount() {
-    this.props.events ? null : this.fetchEvents()
+    this.props.events ? null : this.fetchEvents();
   }
 
   render() {
+    const { slider1ActiveSlide } = this.state;
+
     return (
-      <ScrollView refreshControl={
-        <RefreshControl
-          refreshing={this.state.refreshing}
-          onRefresh={this._onRefresh}
-        />
-      }>
-        {!this.props.isVerified &&
-          <Notification
-            onPress={() => this.props.navigation.navigate('Confirm')}
-            colour='#ff7043'>Please confirm your account</Notification>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
         }
+      >
+        {!this.props.isVerified && (
+          <Notification
+            onPress={() => this.props.navigation.navigate("Confirm")}
+            colour="#ff7043"
+          >
+            Please confirm your account
+          </Notification>
+        )}
         <View style={styles.widgetContainer}>
           <Text style={styles.h1}>Home</Text>
-          {this.props.isLoggedIn && <Text>Welcome, {this.props.userData.fname} </Text>}
+          {this.props.isLoggedIn && (
+            <Text>Welcome, {this.props.userData.fname} </Text>
+          )}
           {!this.props.isLoggedIn && <Text>Welcome to BizTech </Text>}
           <Button
-            title='Event Check-in'
-            onPress={() => this.props.navigation.navigate('Checkin')}
+            title="Event Check-in"
+            onPress={() => this.props.navigation.navigate("Checkin")}
+          />
+          <Carousel
+            ref={c => (this._slider1Ref = c)}
+            data={ENTRIES1}
+            renderItem={this._renderItemWithParallax}
+            sliderWidth={sliderWidth}
+            itemWidth={itemWidth}
+            hasParallaxImages={true}
+            firstItem={SLIDER_1_FIRST_ITEM}
+            inactiveSlideScale={0.94}
+            inactiveSlideOpacity={0.7}
+            // inactiveSlideShift={20}
+            containerCustomStyle={styles.slider}
+            contentContainerCustomStyle={styles.sliderContentContainer}
+            loop={true}
+            loopClonesPerSide={2}
+            autoplay={true}
+            autoplayDelay={500}
+            autoplayInterval={3000}
+            onSnapToItem={index => this.setState({ slider1ActiveSlide: index })}
+          />
+          <Pagination
+            dotsLength={ENTRIES1.length}
+            activeDotIndex={slider1ActiveSlide}
+            containerStyle={styles.paginationContainer}
+            dotColor={"rgba(255, 255, 255, 0.92)"}
+            dotStyle={styles.paginationDot}
+            inactiveDotColor={colors.black}
+            inactiveDotOpacity={0.4}
+            inactiveDotScale={0.6}
+            carouselRef={this._slider1Ref}
+            tappableDots={!!this._slider1Ref}
           />
         </View>
 
@@ -86,12 +155,12 @@ class Home extends Component {
           }
         </View> */}
       </ScrollView>
-    )
+    );
   }
-};
+}
 
 // objects
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     userData: state.login.user,
     isLoggedIn: state.login.isLoggedIn,
@@ -100,10 +169,13 @@ const mapStateToProps = (state) => {
   };
 };
 // actions
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    populateEvents: (events) => dispatch(populateEvents(events))
+    populateEvents: events => dispatch(populateEvents(events))
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
--- App/styles/Styles.js
+++ App/styles/Styles.js
@@ -1,29 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+
+export const colors = {
+  black: '#1a1917',
+  gray: '#888888',
+  background1: '#B721FF',
+  background2: '#21D4FD'
+};
 
 export default styles = StyleSheet.create({
   text: {
-    fontFamily: 'RedHatDisplay-Regular'
+    fontFamily: "RedHatDisplay-Regular"
   },
   bold: {
-    fontFamily: 'RedHatDisplay-Bold'
+    fontFamily: "RedHatDisplay-Bold"
   },
   whitebold: {
-    fontFamily: 'RedHatDisplay-Bold',
-    color: '#ffffff'
+    fontFamily: "RedHatDisplay-Bold",
+    color: "#ffffff"
   },
   colour: {
-    fontFamily: 'RedHatDisplay-Regular',
-    color: '#7ad040'
+    fontFamily: "RedHatDisplay-Regular",
+    color: "#7ad040"
   },
   h1: {
-    fontFamily: 'RedHatDisplay-Bold',
+    fontFamily: "RedHatDisplay-Bold",
     fontSize: 36,
-    lineHeight: 38
+    lineHeight: 38,
+    textAlignVertical: "center",
+    textAlign: "center",
+    paddingTop: 20
   },
   h3: {
-    fontFamily: 'RedHatDisplay-Bold',
+    fontFamily: "RedHatDisplay-Bold",
     fontSize: 18,
-    color: 'black'
+    color: "black"
   },
   widgetContainer: {
     paddingVertical: 25,
@@ -38,68 +48,84 @@ export default styles = StyleSheet.create({
     paddingTop: 0
   },
   center: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
   },
   button: {
-    color: '#fff',
+    color: "#fff",
     padding: 14,
-    backgroundColor: '#7ad040',
+    backgroundColor: "#7ad040",
     marginVertical: 10,
-    borderRadius: 30,
+    borderRadius: 30
   },
   buttonDisabled: {
-    color: '#fff',
+    color: "#fff",
     padding: 14,
-    backgroundColor: '#bbb',
+    backgroundColor: "#bbb",
     marginVertical: 10,
-    borderRadius: 30,
+    borderRadius: 30
   },
   buttonText: {
-    fontFamily: 'RedHatDisplay-Bold',
-    color: '#fff',
-    textAlign: 'center',
+    fontFamily: "RedHatDisplay-Bold",
+    color: "#fff",
+    textAlign: "center"
   },
   card: {
     margin: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundColor: '#ffffff',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "#ffffff",
     paddingBottom: 5,
     borderRadius: 10,
 
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 1,
+      height: 1
     },
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
-    elevation: 4,
+    elevation: 4
   },
 
   loginLogo: {
     width: 80 / 2,
     height: 110 / 2,
-    marginBottom: 10,
+    marginBottom: 10
   },
 
   //form stuff
   container: {
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     paddingLeft: 60,
     paddingRight: 60,
     paddingTop: 40,
     paddingBottom: 60
   },
   input: {
-    fontFamily: 'RedHatDisplay-Regular',
-    color: '#333333',
+    fontFamily: "RedHatDisplay-Regular",
+    color: "#333333",
     paddingVertical: 5,
     marginBottom: 10,
-    borderBottomColor: '#7ad040',
+    borderBottomColor: "#7ad040",
     borderBottomWidth: 1
+  },
+  slider: {
+    marginTop: 15,
+    overflow: "visible" // for custom animations
+  },
+  sliderContentContainer: {
+    paddingVertical: 10 // for custom animation
+  },
+  paginationContainer: {
+    paddingVertical: 8
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 8
   }
-})
+});
